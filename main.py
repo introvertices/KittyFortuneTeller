1b3a7bd2c9af9dae8847f491ff227ab552f60a28
Lannie <artingjunk@gmail.com>
2022-05-24 15:57:05 +1200

    initial version

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from os import name, system
 from time import sleep
-from random import choice
+from random import randint
 import ascii_art as art
 
 def main():
     ### SET UP
-    answers = ["no"]
     clear()
 
     # Prompt the user for a question
     get_input()
 
     # Animate curtains and cat
-    cat_animation(art.curtains)
+    for i in range(1,len(art.curtains)):
+        clear()
+        print(art.curtains[i][0])     
+        sleep(art.curtains[i][1])
 
     # Show last frame of animation and print answer
     clear()
-    print(art.curtains[-1])
-    print(choice(answers))
+    answer = randint(0,len(art.answers)-1)
+    print(art.answers[answer][0])
 
     # Prompt for another question
-    redo = input("Would you like to ask another question? Y/N\n > ")
+    redo = input("\033[1;32;40mWould you like to ask another question? Y/N\n \033[1;37;40m> ")
     if str(redo[0].upper()) == "Y":
         main()
     else:
@@ -34,36 +36,17 @@ def main():
 
 
 def get_input():
-    thanks_responses = ["Hmm, let's see...","I wonder what the fates hold for you?","As you wish..."]
 
     # Print closed curtains
     clear()
-    print(art.curtains[0])
+    print(art.curtains[0][0])
 
     # Ask user for their question, this doesn't need to be stored
-    input("Welcome! What is it your heart desires to know?\n > ")
+    input("\033[1;32;40mWelcome! What is it your heart desires to know?\n \033[1;37;40m> ")
 
-    # clear screen, then thank user
-
-    print(art.curtains[0])
-    print(f"{choice(thanks_responses)}")
-
-
-def cat_animation(ascii_art):
-    ### Setup #########################################################################################
-    clear()
-    
-    # How many times the screen should clear per second. Lower = slower
-    framerate = 12                 
-
-    # Loop to animate the artwork
-    for i in ascii_art:
-        print(i)     
-        sleep(1/framerate)
-        clear()
 
 #!-- HELPERS
-# Clears the screen for ~aesthetics~
+# Clears the screen
 def clear():
     command = 'clear'
     if name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
